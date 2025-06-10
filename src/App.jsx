740ca54ddbcff8b0764c3abf99b90999f6ff9674
Sajeevan <sajeesajeevan1998@gmail.com>
2025-06-10 23:28:52 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./App.css";
+import { LoadingScreen } from "./components/Loading";
+import "./index.css";
+import { Navbar } from "./components/Navbar";
+import { MobileMenu } from "./components/MobileMenu";
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
+      <div
+        className={`min-h-screen transistion-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100 `}
+      >
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      </div>
+    </>
+  );
+}
+
+export default App;
